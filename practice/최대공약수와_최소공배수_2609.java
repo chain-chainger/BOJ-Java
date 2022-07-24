52a598f52d3154768daf162e6f860c0f8cde70b4
chain.chainger <chain.chainger@gmail.com>
2022-07-24 09:55:40 +0900

    22.07.24

--- practice/최대공약수와_최소공배수_2609.java
+++ practice/최대공약수와_최소공배수_2609.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 최소공배수_1934 {
+public class 최대공약수와_최소공배수_2609 {
 	
 	static int GCD(int a, int b) {
 		
@@ -24,18 +24,15 @@ public class 최소공배수_1934 {
 		
 	}
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int t = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < t; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			System.out.println(LCM(a, b));
-		}
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(GCD(a, b));
+		System.out.println(LCM(a, b));
 		
 		br.close();
 		
