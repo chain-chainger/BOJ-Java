2bbe3dd72496af8f519eacc3c78c024690791286
chain.chainger <chain.chainger@gmail.com>
2022-07-26 22:17:44 +0900

    22.07.26

---
+++ practice/마이크로소프트_로고_5338.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class 마이크로소프트_로고_5338 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+		
+	}
+
+}
