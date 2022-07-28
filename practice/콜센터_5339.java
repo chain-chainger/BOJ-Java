4aeeee6a3661cbd7a7bf5f639c95477de918e9b4
chain.chainger <chain.chainger@gmail.com>
2022-07-29 08:47:17 +0900

    22.07.29

---
+++ practice/콜센터_5339.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class ÄÝ¼¾ÅÍ_5339 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		
+	}
+
+}
