864018766a8f478dcd83d59b6d2518b2b65056f3
chain.chainger <chain.chainger@gmail.com>
2022-08-04 11:47:13 +0900

    22.08.04

---
+++ practice/얼마_9325.java
@@ -0,0 +1,35 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ¾ó¸¶_9325 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		int test = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<test; i++) {
+			int s = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			for(int j=0; j<n; j++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int q = Integer.parseInt(st.nextToken());
+				int p = Integer.parseInt(st.nextToken());
+				s += q * p;
+			}
+			sb.append(s + "\n");
+		}
+		
+		System.out.print(sb);
+		
+		br.close();
+		
+	}
+
+}
