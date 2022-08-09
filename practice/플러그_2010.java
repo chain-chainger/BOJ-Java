82a989fd7aa22d8411969911bad272ec221d102a
chain.chainger <chain.chainger@gmail.com>
2022-08-09 11:40:46 +0900

    22.08.09

---
+++ practice/플러그_2010.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ÇÃ·¯±×_2010 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int sum = 0;
+		
+		for(int i=0; i<n; i++) {
+			sum += Integer.parseInt(br.readLine());
+		}
+		
+		System.out.println(sum - (n - 1));
+		
+		br.close();
+		
+	}
+
+}
